4b18ae05315477c1afc7f2abfbd806110ee02b64
dangduyquyen <dangduyquyensv@gmail.com>
2021-09-08 10:53:00 +0700

    Init project LopHocJavaCoBan

---
+++ src/bai_1/hello_world.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai_1;
+
+/**
+ *
+ * @author Admin
+ */
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println("hello world. ");
+    }
+}
