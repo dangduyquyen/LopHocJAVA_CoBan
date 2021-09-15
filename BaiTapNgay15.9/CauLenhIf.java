e973ef8dc6a3368c405562a3eaf75ca0331b4d0a
dangduyquyen <dangduyquyensv@gmail.com>
2021-09-15 14:46:26 +0700

    add bai tap

---
+++ BaiTapNgay15.9/CauLenhIf.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session3;
+
+/**
+ *
+ * @author Admin
+ */
+public class CauLenhIf {
+    public static void main(String[] args) {
+        System.out.println("Buổi học thứ 3");
+        if(1>2){
+            System.out.println("Điều kiện đúng");
+        }
+        System.out.println("Điều kiện sai");
+    }
+    
+}
