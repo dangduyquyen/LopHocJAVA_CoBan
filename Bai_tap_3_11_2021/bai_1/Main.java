1b83f32b7f380e50f9271678a86dba5cd1cff733
dangduyquyen <dangduyquyensv@gmail.com>
2021-11-03 18:51:27 +0700

    add bai tap

---
+++ Bai_tap_3_11_2021/bai_1/Main.java
@@ -0,0 +1,41 @@
+package bai_1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		File myObj = new File("Test_bai_1.txt");
+		
+		try {
+		      FileWriter myWriter = new FileWriter("Test_bai_1.txt");
+		      myWriter.write(str);
+		      myWriter.close();
+		      System.out.println("Successfully wrote to the file.");
+		    } catch (IOException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+		
+		try {
+//		      File myObj = new File("Test_bai_1.txt");
+		      Scanner myReader = new Scanner(myObj);
+		      while (myReader.hasNextLine()) {
+		        String data = myReader.nextLine();
+		        System.out.println(data);
+		      }
+		      myReader.close();
+		    } catch (FileNotFoundException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+	}
+
+}
