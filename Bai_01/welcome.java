40bf9beb8f60e21cd92ca493a4ea7670cb64ede4
dangduyquyen <83438285+dangduyquyen@users.noreply.github.com>
2022-01-14 16:50:48 +0700

    add bai 01

--- Bai_01/welcome.java
+++ Bai_01/welcome.java
@@ -1,10 +1,7 @@
 package Bai_01;
 
 public class welcome {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to java");
+	public static void main(String[] agsr) {
+		System.out.print("Welcome to java");
 	}
-
 }
--- Bai_01/welcome_txt.java
+++
@@ -1,8 +0,0 @@
-public class welcome {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to java");
-	}
-
-}
